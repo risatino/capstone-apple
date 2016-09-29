883ad1aa719846ae400ea0598d3b52494adf1959
risatino <charissatolentino@gmail.com>
2016-09-28 20:48:30 -0500

    updates to documents controller and views

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -20,6 +20,25 @@ class DocumentsController < ApplicationController
   end
 
   def edit
+    @document = Document.find(params[:id])
+  end
+
+  def update
+      @document = Document.find(params[:id])
+      @document.update(contract_type: params[:contract_type],
+                      image: params[:image],
+                      user_id: params[:user_id]
+                      )
+
+      flash[:success] = "Your data has been updated."
+      redirect_to "/documents/#{@document.id}"  
+  end
+
+  def destroy
+    @document = Document.find(params[:id])
+    @document.destroy
     
+    flash[:warning] = "And...your data...she gone...bai bai."
+    redirect_to '/documents' 
   end
 end
