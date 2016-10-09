9245d7895fd8f2be1fa3fe58da5bd948564d319e
risatino <charissatolentino@gmail.com>
2016-10-08 22:18:35 -0500

    updates to documents form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/' => 'documents#index'
   get '/documents' => 'documents#index'
   get '/documents/new' => 'documents#new'
   post '/documents' => 'documents#create'
